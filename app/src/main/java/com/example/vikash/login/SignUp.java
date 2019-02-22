24bfca84b1ec07aff6a9fabb6854f9c27ac5a87a
VIKASH <kumarv4624@gmail.com>
2019-02-22 17:19:27 +0530

    Constraint on name and mobile number

--- app/src/main/java/com/example/vikash/login/SignUp.java
+++ app/src/main/java/com/example/vikash/login/SignUp.java
@@ -21,7 +21,8 @@ public class SignUp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up); editText1 = (EditText)findViewById(R.id.t1);
+        setContentView(R.layout.activity_sign_up);
+        editText1 = (EditText)findViewById(R.id.t1);
         editText2 = (EditText)findViewById(R.id.t2);
         editText3 = (EditText)findViewById(R.id.t3);
         editText4 = (EditText)findViewById(R.id.t4);
@@ -45,6 +46,10 @@ public class SignUp extends AppCompatActivity {
                 String email = editText4.getText().toString().trim();
                 String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
+                String firstName = editText1.getText().toString().trim();
+                String lastname = editText2.getText().toString().trim();
+                String matchName = "[a-zA-Z]";
+
                 if (str1.matches("") || str2.matches("") || str3.matches("")
                         || str4.matches("") || str5.matches("") || str6.matches(""))
                 {
@@ -54,46 +59,57 @@ public class SignUp extends AppCompatActivity {
 
                 else {
 
-                    if (str4.length()<10)
-                    {
-                        Toast.makeText(getApplicationContext(), "Enter valid mobile number", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
+                    if(firstName.matches(matchName) && lastname.matches(matchName) ) {
+
+                        if (str4.length() < 10) {
+                            Toast.makeText(getApplicationContext(), "Enter valid mobile number", Toast.LENGTH_SHORT).show();
+                        } else {
 
 
-                        if (email.matches(emailPattern)) {
+                            if (email.matches(emailPattern)) {
+
+                                if(str1.length()<8 && str2.length()<8) {
 
 
-                            if (str1.equals(str2)) {
-                                startActivity(new Intent(getApplicationContext(), com.example.vikash.login.progressbar.class));
+                                    if (str1.equals(str2)) {
+                                        startActivity(new Intent(getApplicationContext(), com.example.vikash.login.progressbar.class));
 
-                                final Handler handler = new Handler();
-                                handler.postDelayed(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Toast.makeText(SignUp.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
-                                        editText1.setText("");
-                                        editText2.setText("");
-                                        editText3.setText("");
-                                        editText4.setText("");
-                                        editText5.setText("");
-                                        editText6.setText("");
+                                        final Handler handler = new Handler();
+                                        handler.postDelayed(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                Toast.makeText(SignUp.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                                                editText1.setText("");
+                                                editText2.setText("");
+                                                editText3.setText("");
+                                                editText4.setText("");
+                                                editText5.setText("");
+                                                editText6.setText("");
 
+                                            }
+                                        }, 3000);
+
+
+                                    } else {
+                                        Toast.makeText(SignUp.this, "Password does not match", Toast.LENGTH_SHORT).show();
                                     }
-                                }, 3000);
+                                }
+                                else
+                                {
+                                    Toast.makeText(getApplicationContext(), "Min 8 characters", Toast.LENGTH_SHORT).show();
+                                }
 
 
-                            } else {
-                                Toast.makeText(SignUp.this, "Password does not match", Toast.LENGTH_SHORT).show();
+                            } else
+
+                            {
+                                Toast.makeText(getApplicationContext(), "Invalid Email Address", Toast.LENGTH_SHORT).show();
+
                             }
-
-
-                        } else
-
-                        {
-                            Toast.makeText(getApplicationContext(), "Invalid Email Password", Toast.LENGTH_SHORT).show();
-
                         }
+                    }else
+                    {
+                        Toast.makeText(getApplicationContext(), "Name contains alphabets only", Toast.LENGTH_SHORT).show();
                     }
                 }
 
