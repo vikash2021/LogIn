5f384ab29e7a757e6ea61c00179b50578801a6db
VIKASH <kumarv4624@gmail.com>
2019-02-23 16:06:30 +0530

    Login layout

--- app/src/main/java/com/example/vikash/login/SignUp.java
+++ app/src/main/java/com/example/vikash/login/SignUp.java
@@ -47,8 +47,8 @@ public class SignUp extends AppCompatActivity {
                 String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
                 String firstName = editText1.getText().toString().trim();
-                String lastname = editText2.getText().toString().trim();
-                String matchName = "[a-zA-Z]";
+                String lastName = editText2.getText().toString().trim();
+                String matchName = "[a-zA-Z]+";
 
                 if (str1.matches("") || str2.matches("") || str3.matches("")
                         || str4.matches("") || str5.matches("") || str6.matches(""))
@@ -59,57 +59,62 @@ public class SignUp extends AppCompatActivity {
 
                 else {
 
-                    if(firstName.matches(matchName) && lastname.matches(matchName) ) {
+                    if(firstName.matches(matchName)  ) {
 
-                        if (str4.length() < 10) {
-                            Toast.makeText(getApplicationContext(), "Enter valid mobile number", Toast.LENGTH_SHORT).show();
-                        } else {
+                        if(lastName.matches(matchName)) {
 
-
-                            if (email.matches(emailPattern)) {
-
-                                if(str1.length()<8 && str2.length()<8) {
+                            if (str4.length() < 10) {
+                                Toast.makeText(SignUp.this, "Enter valid mobile number", Toast.LENGTH_SHORT).show();
+                            } else {
 
 
-                                    if (str1.equals(str2)) {
-                                        startActivity(new Intent(getApplicationContext(), com.example.vikash.login.progressbar.class));
+                                if (email.matches(emailPattern)) {
 
-                                        final Handler handler = new Handler();
-                                        handler.postDelayed(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                                Toast.makeText(SignUp.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
-                                                editText1.setText("");
-                                                editText2.setText("");
-                                                editText3.setText("");
-                                                editText4.setText("");
-                                                editText5.setText("");
-                                                editText6.setText("");
-
-                                            }
-                                        }, 3000);
+                                    if ((str1.length() < 8) && (str2.length() < 8)) {
 
 
+                                        if (str1.equals(str2)) {
+                                            startActivity(new Intent(getApplicationContext(), com.example.vikash.login.progressbar.class));
+
+                                            final Handler handler = new Handler();
+                                            handler.postDelayed(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    Toast.makeText(SignUp.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                                                    editText1.setText("");
+                                                    editText2.setText("");
+                                                    editText3.setText("");
+                                                    editText4.setText("");
+                                                    editText5.setText("");
+                                                    editText6.setText("");
+
+                                                }
+                                            }, 3000);
+
+
+                                        } else {
+                                            Toast.makeText(SignUp.this, "Password does not match", Toast.LENGTH_SHORT).show();
+                                        }
                                     } else {
-                                        Toast.makeText(SignUp.this, "Password does not match", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(SignUp.this, "Min 8 characters", Toast.LENGTH_SHORT).show();
                                     }
-                                }
-                                else
+
+
+                                } else
+
                                 {
-                                    Toast.makeText(getApplicationContext(), "Min 8 characters", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(SignUp.this, "Invalid Email Address", Toast.LENGTH_SHORT).show();
+
                                 }
-
-
-                            } else
-
-                            {
-                                Toast.makeText(getApplicationContext(), "Invalid Email Address", Toast.LENGTH_SHORT).show();
-
                             }
+                        }else
+                        {
+                            Toast.makeText(SignUp.this, "Name contains alphabets only", Toast.LENGTH_SHORT).show();
                         }
+
                     }else
                     {
-                        Toast.makeText(getApplicationContext(), "Name contains alphabets only", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SignUp.this, "Name contains alphabets only", Toast.LENGTH_SHORT).show();
                     }
                 }
 
